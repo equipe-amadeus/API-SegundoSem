86c785159e6da833a38bfc2a57a167cb25d49b0b
Gustavo Lobato de Paulo <88914313+Gustavoldp@users.noreply.github.com>
2022-04-04 12:54:23 -0300

    Tela-Registro-Mensagem

--- ProjetoPro4Tech/src/javafxapplication4/FXMLDocumentController.java
+++ ProjetoPro4Tech/src/javafxapplication4/FXMLDocumentController.java
@@ -4,31 +4,22 @@
  */
 package javafxapplication4;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-/**
- *
- * @author lobat
- */
-public class FXMLDocumentController implements Initializable {
-    
+public class FXMLDocumentController {
+
+    @FXML
+    private Button button;
+
     @FXML
     private Label label;
-    
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    void handleButtonAction(ActionEvent event) {
+
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
 }
