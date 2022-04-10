fe1c673dbad7381a8309d14519cb7fc37c264b6e
Gustavo Lobato de Paulo <88914313+Gustavoldp@users.noreply.github.com>
2022-04-10 14:40:55 -0300

    Alterações de design

---
+++ Pro4Tech/src/controllers/TelaMensagensCadastradasController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TelaMensagensCadastradasController {
+
+    @FXML
+    private Button botao2;
+
+    @FXML
+    void cadastrarCliente(ActionEvent event) {
+
+    }
+
+}
+
