0dea82237bf0c13e88438f87e75b74d5ed9bd78d
Gustavo Lobato de Paulo <88914313+Gustavoldp@users.noreply.github.com>
2022-05-17 00:50:20 -0300

    Corrigindo erros

--- Pro4Tech/src/controllers/TelaChatController.java
+++ Pro4Tech/src/controllers/TelaChatController.java
@@ -8,8 +8,16 @@ import Dao.CadastroChatDAO;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 import modelo.CadastroChat;
 import view.ViewFactory;
@@ -24,6 +32,11 @@ public class TelaChatController extends BaseController{
         super(viewFactory, fxmlName);
     }
     
+    @FXML
+    private VBox Vbox;
+    
+    @FXML
+    private ScrollPane SP;
     
     @FXML
     private TextArea MessageBox;
@@ -33,6 +46,9 @@ public class TelaChatController extends BaseController{
 
     @FXML
     private Button voltar;
+    
+    
+    
 
 
     @FXML
@@ -46,18 +62,36 @@ public class TelaChatController extends BaseController{
         
         CadastroChat chat = new CadastroChat();
         
-        
+        if(! mensagem.isEmpty()){
         chat.setRemetente(remetente);
         chat.setMensagem(mensagem);
         chat.setDestinatario(destinatario);
         
         CadastroChatDAO objCadastroChat = new CadastroChatDAO();
+  
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.TOP_RIGHT);
+        
+        hBox.setPadding(new Insets(5, 5, 5, 10));
+        Text text = new Text(mensagem);
+        TextFlow textFlow = new TextFlow(text);
+        textFlow.setStyle("-fx-color: rgb(239,242,255);" + 
+                "-fx-background-color: rgb(15,125,242);" + 
+                " -fx-background-radius: 20px;");
+        
+        textFlow.setPadding(new Insets(5, 10, 5, 10));
+        text.setFill(Color.color(0.934, 0.945, 0.996));
+        
+        hBox.getChildren().add(textFlow);
+        Vbox.getChildren().add(hBox);
+        
+        
         objCadastroChat.Cadastrar(chat);
         
         MessageBox.clear();
         
+        }    
     }
-    
     
     @FXML
     void Voltar(ActionEvent event) {
